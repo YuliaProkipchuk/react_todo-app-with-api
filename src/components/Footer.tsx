4df85aa55a9833a6c175854f97bcdaa967e326dd
YuliaProkipchuk <yulayulayula2003@gmail.com>
2025-09-01 10:05:41 +0300

    remove comments

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -43,38 +43,6 @@ function Footer({
           value={Filters.Completed}
           onChangeFilter={onChangeFilter}
         />
-        {/* <a
-          href="#/"
-          className={classNames('filter__link', {
-            selected: filter === Filters.All,
-          })}
-          data-cy="FilterLinkAll"
-          onClick={() => onChangeFilter(Filters.All)}
-        >
-          All
-        </a>
-
-        <a
-          href="#/active"
-          className={classNames('filter__link', {
-            selected: filter === Filters.Active,
-          })}
-          data-cy="FilterLinkActive"
-          onClick={() => onChangeFilter(Filters.Active)}
-        >
-          Active
-        </a>
-
-        <a
-          href="#/completed"
-          className={classNames('filter__link', {
-            selected: filter === Filters.Completed,
-          })}
-          data-cy="FilterLinkCompleted"
-          onClick={() => onChangeFilter(Filters.Completed)}
-        >
-          Completed
-        </a> */}
       </nav>
 
       <button
